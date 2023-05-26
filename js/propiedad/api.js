17fdb245ac393cbe91330cd2fc56eda2c40badd3
JaackX1 <nicolascabreramillacan@gmail.com>
2023-05-26 17:36:17 -0400

    feat: adding code in properties

--- js/propiedad/api.js
+++ js/propiedad/api.js
@@ -73,7 +73,11 @@ export default async function apiCall() {
                     <i class="bi bi-pin-map"></i> ${data.commune != null && data.commune != undefined && data.commune != "" ? data.commune : "No registra comuna"}, ${data.region != null && data.region != undefined && data.region != "" ? data.region : "No registra Región"}, Chile
                   </p>
                 </div>
-              
+                <div class="">
+                  <p class="text-center">
+                    COD: ${data.id}
+                  </p>
+                </div>
                 <div class="mt-5 d-flex">
                   <h4 class="flex-fill text-center py-2">
                     <b>UF ${clpToUf(data.price,ufValueAsNumber)}</b>
@@ -128,6 +132,11 @@ export default async function apiCall() {
 												<div class="p-4 pb-0 card-props">
 													<p class="text-center">
 														<i class="bi bi-pin-map"></i> ${data.commune != null && data.commune != undefined && data.commune != "" ? data.commune : "No registra comuna"}, ${data.region != null && data.region != undefined && data.region != "" ? data.region : "No registra Región"}, Chile
+													</p>
+												</div>
+                        <div class="">
+													<p class="text-center">
+														COD: ${data.id}
 													</p>
 												</div>
 												<div class="d-flex">
